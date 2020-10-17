8354f642432b32b6b8346379ac802593db800eb3
pallavi <pallavirichhariya24@gmail.com>
2020-10-17 23:59:06 +0530

    changes

--- app/src/main/java/com/example/theweddingcard/FirstFragment.kt
+++ app/src/main/java/com/example/theweddingcard/FirstFragment.kt
@@ -1,24 +1,32 @@
 package com.example.theweddingcard
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import java.util.*
+
+
 class FirstFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//        view.findViewById<TextView>(R.id.mapLink).setOnClickListener {
+//            val uri: String =
+//                java.lang.String.format(Locale.ENGLISH, "geo:%f,%f", latitude, longitude)
+//            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+//            context!!.startActivity(intent)
 //        }
     }
 
